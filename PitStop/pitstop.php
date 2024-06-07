e216f7165f5fe5134a52126af780d880cd002e8f
Vikthor <dev.joao.victor@outlook.com>
2024-06-07 09:25:39 -0300

    Add files via upload

--- PitStop/pitstop.php
+++ PitStop/pitstop.php
@@ -4,10 +4,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PitStop</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
 </head>
 
 <body>
+
+<div class="stop">
+      <h1>PitStop System Calculator <box-icon name='flag-checkered' type='solid' ></box-icon></h1> 
+
+    <div class="pit">
+        <form method="get" action="pitstop.php">
+            <h3>Comprimento de Pista</h3>
+            <input type="number" name="comprimento_pista" placeholder="comprimento de Pista" required step="0.001">
+            <h3>Total de Voltas a serem Percorridas</h3>
+            <input type="number" name="total_voltas" placeholder="Total de Voltas " required
+                step="0.001">
+            <h3>Reabastecimentos desejados</h3>
+            <input type="number" name="num_reabastecimentos" placeholder="Reabastecimentos " required
+                step="0.001">
+            <h3>Consumo de Combustivel</h3>
+            <input type="number" name="consumo_carro" placeholder="Consumo  " required step="0.001"> <br>
+            <br>
+            <input type="submit">
+        </form>
+        
+
+    </div>
+</div>
 
     <?php
 
@@ -33,10 +57,11 @@
     //  Converter para inteiro 
     $litros_formatados = $litros_necessarios*1000;
 
-    echo "Número mínimo de litros necessários para percorrer até o primeiro reabastecimento: " .( round($litros_formatados));
-    
     ?>
+  <br>  
+<textarea name="" id="" cols="50" ><?php echo " Número mínimo de litros necessários para percorrer até o primeiro reabastecimento: " .( round($litros_formatados)); ?></textarea>
 
+<script src="https://unpkg.com/boxicons@2.1.3/dist/boxicons.js"></script>
 </body>
 
 </html>
